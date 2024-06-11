1aebce122544995283de71836b8c4a31682b2ae3
Ryan <tangguangran@gmail.com>
2024-06-11 14:09:36 -0500

    more description fixes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -443,7 +443,7 @@ export default function Home() {
                 <div className="absolute top-0 left-0 w-full h-full bg-[#1C1D25] opacity-0 group-hover:opacity-70 transition-opacity duration-700 ease rounded-sm"></div>
                 <div className="absolute left-1/2 transform -translate-x-1/2 max-w-[550px] w-full px-2 text-center transition-all duration-700 ease opacity-0 group-hover:opacity-100 group-hover:top-1/2 group-hover:-translate-y-1/2 top-3/4 text-md sm:text-xl text-white">
                   <p>Tesla clone with login functionality</p>
-                  <p>React, Firebase, Auth</p>
+                  <p>React, Firebase, Redux, Auth</p>
                 </div>
                 <div className="absolute left-1/2 transform -translate-x-1/2 max-w-[550px] w-full px-2 text-center transition-all duration-700 ease opacity-0 group-hover:opacity-100 group-hover:top-3/4 group-hover:-translate-y-1/2 top-3/4 text-white flex flex-row items-center justify-center gap-4">
                   <Button
@@ -487,7 +487,7 @@ export default function Home() {
                 <div className="absolute top-0 left-0 w-full h-full bg-[#1C1D25] opacity-0 group-hover:opacity-70 transition-opacity duration-700 ease rounded-sm"></div>
                 <div className="absolute left-1/2 transform -translate-x-1/2 max-w-[550px] w-full px-2 text-center transition-all duration-700 ease opacity-0 group-hover:opacity-100 group-hover:top-1/2 group-hover:-translate-y-1/2 top-3/4 text-md sm:text-xl text-white">
                   <p>ACME Dashboard for tracking transactions.</p>
-                  <p> Next.JS, React, Auth, Vercel DB</p>
+                  <p> Next.JS, React, Auth, Redux, Vercel DB</p>
                 </div>
                 <div className="absolute left-1/2 transform -translate-x-1/2 max-w-[550px] w-full px-2 text-center transition-all duration-700 ease opacity-0 group-hover:opacity-100 group-hover:top-3/4 group-hover:-translate-y-1/2 top-3/4 text-white flex flex-row items-center justify-center gap-4">
                   <Button
